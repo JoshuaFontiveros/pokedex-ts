d5dc14c0f0d52e591eff406660542053392beea7
Joshua Fontiveros <fontiveros.joshua@gmail.com>
2022-06-28 01:10:20 +0800

    Fix: Remove unnecessary codes.

--- src/routes/poketype/usePoketype.tsx
+++ src/routes/poketype/usePoketype.tsx
@@ -7,18 +7,8 @@ export interface IPoketype {
   }[];
 }
 
-interface IPoketypeNamesOnly {
-  poketypeNames: {
-    name: string;
-  }[];
-}
-
 const usePoketype = () => {
   const [poketype, setPoketype] = useState<IPoketype["poketype"]>([]);
-
-  const [poketypeNames, setPoketypeNames] = useState<
-    IPoketypeNamesOnly["poketypeNames"]
-  >([]);
 
   const getPoketypes = async () => {
     await axios.get(`https://pokeapi.co/api/v2/type`).then((res) => {
